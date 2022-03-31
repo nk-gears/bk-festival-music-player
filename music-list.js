4e91337c3f685234df87909a1fd03c0b596983ca
Nirmal <nirmal_kumar@live.in>
2022-03-31 11:20:34 +0530

    initial commit

---
+++ music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [{
+        name: "Hindi -  Shiv ka jhanda Oonch Mahaan",
+        id:"m1",
+        artist: "Shivjayanthi Songs",
+        img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
+        src: "https://omshantimusic.s3.amazonaws.com/upload/audio/2022/02/MKIUXdPbkDbuMp7jZ5ue_24_482e2b9371896087b76bf9c04e028446_audio.mp3"
+    },
+    {
+        name: "Hindi -  Panchi Re - Bhog Song",
+        id:"m2",
+        artist: "Shivjayanthi Songs",
+        img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
+        src: "https://bkdivinesongs.in/1/29%20Pabhu%20Milan/02%20Panchi%20Re%20Ud%20Jaa.mp3"
+    },
+    {
+        name: "Hindi -  Teri Yaad Ka Amrit - Bhog Song",
+        artist: "Shivjayanthi Songs",
+        id:"m3",
+        img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
+        src: "https://bkdivinesongs.in/1/29%20Pabhu%20Milan/11%20Teri%20Yaad%20Ka%20Amrut.mp3"
+    },
+    {
+        name: "Tamil -  Kandu Kondaein (கண்டுகொண்டேன் கண்டுகொண்டேன் ) - Bhog Song",
+        artist: "Shivjayanthi Songs",
+        id:"m4",
+        img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
+        src: "http://docs.google.com/uc?export=open&id=1Zmkrribw_TnTiyM21-lfhXMsJl6Rrpzj"
+    }];
+
+    
+    
+    
