d04a9f40060bb5ed748c64ac721138d30c2944a2
Nirmal <nirmal_kumar@live.in>
2022-04-01 16:37:09 +0530

    updated files

--- music-list.js
+++ music-list.js
@@ -20,12 +20,63 @@ let allMusic = [{
         src: "https://bkdivinesongs.in/1/29%20Pabhu%20Milan/11%20Teri%20Yaad%20Ka%20Amrut.mp3"
     },
     {
-        name: "Tamil -  Kandu Kondaein (கண்டுகொண்டேன் கண்டுகொண்டேன் ) - Bhog Song",
+        name: "Tamil -  கண்டுகொண்டேன் கண்டுகொண்டேன் ",
         artist: "Shivjayanthi Songs",
         id:"m4",
         img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
         src: "http://docs.google.com/uc?export=open&id=1Zmkrribw_TnTiyM21-lfhXMsJl6Rrpzj"
-    }];
+    },
+    {
+        name: "Tamil -  இராஜயோகம் பயின்றிடுவோம் ",
+        artist: "Other Songs",
+        id:"m5",
+        img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
+        src: "http://docs.google.com/uc?export=open&id=1h00k3telcaneA-f9XcciznXs451J6Doq"
+
+    },
+    {
+
+        name: "Tamil -  சதா சிவம் உன் நாமம் ",
+        artist: "Other Songs",
+        id:"m6",
+        img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
+        src: "http://docs.google.com/uc?export=open&id=1i47gb3JALYBNxeNdPX4z5ntYMy3eeaGd"
+
+    },
+    {
+        name: "Tamil -  பரமாத்மாவே சிவ பகவானே",
+        artist: "Other Songs",
+        id:"m7",
+        img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
+        src: "http://docs.google.com/uc?export=open&id=1Sij4AbOG2hOGLbmZGAdpmDMVxWCO45Gj"
+
+    },
+    {
+        name: "Tamil -  சிவலோக நாதன்",
+        artist: "Other Songs",
+        id:"m8",
+        img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
+        src: "http://docs.google.com/uc?export=open&id=1Amxc97mPiXwdGXaEMb1jZdG0qRYIY4wz"
+
+    },
+    {
+        name: "Tamil -  சுடர்விடும் ஜோதியிலே",
+        artist: "Other Songs",
+        id:"m9",
+        img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
+        src: "http://docs.google.com/uc?export=open&id=1_eth54hymrFk9mai3_4BdzTxkuy7jRnG"
+
+    },
+    {
+        name: "Tamil -  ஆத்ம தீபம் ஏற்றிவிடு",
+        artist: "Other Songs",
+        id:"m9",
+        img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
+        src: "http://docs.google.com/uc?export=open&id=1TRDOxNxw0Dyf1LtXUS_EQ0onV4m0l2TV"
+
+    }
+
+];
 
     
     
