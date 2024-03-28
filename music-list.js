7e87b8dbb7016369560d178e51afbc99782f5797
Nirmal <nirmal_kumar@live.in>
2024-03-28 16:17:30 +0530

    cleanup

--- music-list.js
+++ music-list.js
@@ -1,13 +1,32 @@
-let allMusic = [{
-        name: "Hindi -  Shiv ka jhanda Oonch Mahaan",
+let allMusic = [
+    { 
+        name: "Hindi -  Swagatam",
         id:"m1",
+        artist: "Shivjayanthi Songs",
+        img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
+        src: "https://s3.ap-southeast-1.wasabisys.com/omshantimusic/upload/audio/2021/08/Bai2TkVg8OslLGlwuBfu_17_280e8c971f7e25b3ab129241c9394ce8_audio.mp3"
+
+        
+    },
+
+    {
+        name: "Hindi -  Shiv ka jhanda Oonch Mahaan",
+        id:"m2",
         artist: "Shivjayanthi Songs",
         img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
         src: "https://omshantimusic.s3.amazonaws.com/upload/audio/2022/02/MKIUXdPbkDbuMp7jZ5ue_24_482e2b9371896087b76bf9c04e028446_audio.mp3"
     },
+    
+    {
+        name: "Tamil - பரமாத்மா சிவனின் பொன்னான கொடியை",
+        id:"m3",
+        artist: "Shivjayanthi Songs",
+        img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
+        src: "https://s3.ap-southeast-1.wasabisys.com/omshantimusic/upload/audio/2022/09/hdL9pU5lCI4UFvApcwa2_21_5f39471cee3718e69f623ee145d8d696_audio.mp3"
+    },
     {
         name: "Hindi -  Panchi Re - Bhog Song",
-        id:"m2",
+        id:"m4",
         artist: "Shivjayanthi Songs",
         img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
         src: "https://bkdivinesongs.in/1/29%20Pabhu%20Milan/02%20Panchi%20Re%20Ud%20Jaa.mp3"
@@ -15,21 +34,21 @@ let allMusic = [{
     {
         name: "Hindi -  Teri Yaad Ka Amrit - Bhog Song",
         artist: "Shivjayanthi Songs",
-        id:"m3",
+        id:"m5",
         img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
         src: "https://bkdivinesongs.in/1/29%20Pabhu%20Milan/11%20Teri%20Yaad%20Ka%20Amrut.mp3"
     },
     {
         name: "Tamil -  கண்டுகொண்டேன் கண்டுகொண்டேன் ",
         artist: "Shivjayanthi Songs",
-        id:"m4",
+        id:"m6",
         img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
         src: "http://docs.google.com/uc?export=open&id=1Zmkrribw_TnTiyM21-lfhXMsJl6Rrpzj"
     },
     {
         name: "Tamil -  இராஜயோகம் பயின்றிடுவோம் ",
         artist: "Other Songs",
-        id:"m5",
+        id:"m7",
         img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
         src: "http://docs.google.com/uc?export=open&id=1h00k3telcaneA-f9XcciznXs451J6Doq"
 
@@ -38,7 +57,7 @@ let allMusic = [{
 
         name: "Tamil -  சதா சிவம் உன் நாமம் ",
         artist: "Other Songs",
-        id:"m6",
+        id:"m8",
         img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
         src: "http://docs.google.com/uc?export=open&id=1i47gb3JALYBNxeNdPX4z5ntYMy3eeaGd"
 
@@ -46,7 +65,7 @@ let allMusic = [{
     {
         name: "Tamil -  பரமாத்மாவே சிவ பகவானே",
         artist: "Other Songs",
-        id:"m7",
+        id:"m9",
         img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
         src: "http://docs.google.com/uc?export=open&id=1Sij4AbOG2hOGLbmZGAdpmDMVxWCO45Gj"
 
@@ -54,7 +73,7 @@ let allMusic = [{
     {
         name: "Tamil -  சிவலோக நாதன்",
         artist: "Other Songs",
-        id:"m8",
+        id:"m10",
         img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
         src: "http://docs.google.com/uc?export=open&id=1Amxc97mPiXwdGXaEMb1jZdG0qRYIY4wz"
 
@@ -62,7 +81,7 @@ let allMusic = [{
     {
         name: "Tamil -  சுடர்விடும் ஜோதியிலே",
         artist: "Other Songs",
-        id:"m9",
+        id:"m11",
         img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
         src: "http://docs.google.com/uc?export=open&id=1_eth54hymrFk9mai3_4BdzTxkuy7jRnG"
 
@@ -70,7 +89,7 @@ let allMusic = [{
     {
         name: "Tamil -  ஆத்ம தீபம் ஏற்றிவிடு",
         artist: "Other Songs",
-        id:"m9",
+        id:"m12",
         img: "https://omshantimusic.s3.amazonaws.com/upload/photos/2022/02/sl1cTF69zPI1OJiqpDLR_24_c8192170f6c150d1ebce70d242fa4e7b_image.jpg",
         src: "http://docs.google.com/uc?export=open&id=1TRDOxNxw0Dyf1LtXUS_EQ0onV4m0l2TV"
 
